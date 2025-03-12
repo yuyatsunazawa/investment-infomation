1a4f1ffa9e0e2a7b34480d1fc3aeeefe16ec3a85
yuyatsunazawa <36617202+yuyatsunazawa@users.noreply.github.com>
2025-03-12 23:27:06 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["example.com"], // 必要なら画像のドメイン許可
+  },
+};
+
+module.exports = nextConfig;
