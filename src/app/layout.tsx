02f941756b318c56b60036f62d79b5e3175245a2
yuyatsunazawa <36617202+yuyatsunazawa@users.noreply.github.com>
2025-03-12 23:27:34 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,10 @@
 
+import "./styles.css";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body className="p-4 bg-gray-50">{children}</body>
+    </html>
+  );
+}
